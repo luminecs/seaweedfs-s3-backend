9b62a15149f5a52cca3431f42c73121b2796ac2c
luminecs <wangxuke.china@gmail.com>
2025-06-18 13:18:33 +0800

    commit

---
+++ src/main/java/com/yourcompany/seaweedfs/SeaweedfsS3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.yourcompany.seaweedfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaweedfsS3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaweedfsS3BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourcompany/seaweedfs/SeaweedfsS3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcompany.seaweedfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaweedfsS3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
